b66e1bd69527774121b051a046decc64b72b9a39
Sami MOURAD <33941598+Sami2EaCOS@users.noreply.github.com>
2018-11-30 14:55:07 +0100

    Update stateManager.hpp

--- stateManager.hpp
+++ stateManager.hpp
@@ -1,4 +1,19 @@
 #ifndef STATEMANAGER_HPP
 #define STATEMANAGER_HPP
 
+#include <SFML/Graphics.hpp>
+
+class StateManager {
+
+private:
+    int m_current_view;
+    
+public:
+    StateManager();
+	void update(sf::Time& time);
+	void render(sf::RenderWindow& window);
+	void process_event(sf::Event& event, sf::Time& time);
+	void changeView(int new_view);
+}
+
 #endif // STATEMANAGER_HPP
