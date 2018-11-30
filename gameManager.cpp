c6245aeaa51d1393270e07799ca33dfd0ab8ddc3
Sami MOURAD <33941598+Sami2EaCOS@users.noreply.github.com>
2018-11-30 14:35:39 +0100

    Create gameManager.cpp

---
+++ gameManager.cpp
@@ -0,0 +1,32 @@
+#include "gameManager.hpp"
+#include <SFML/Graphics.hpp>
+#include <ctime>
+
+#include "definition.hpp"
+
+GameManager :: GameManager() : m_time(m_clock.restart()) {
+    m_settings.antialiasingLevel = 8;
+    m_window.create(
+        sf::VideoMode(VIDEOWIDTH,VIDEOHEIGHT),
+        "OP4",
+        sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize,
+        m_settings);
+}
+
+void GameManager :: run() {
+    while (m_window.isOpen()) {
+        while (m_window.pollEvent(m_event)) {
+            switch(m_event.type) {
+            case sf::Event::Closed:
+                m.window.close();
+                break;
+            case sf::Event::Resized:
+                break;
+            default:
+                break;
+            }
+        }
+        m_window.clear(sf::Color::Black);
+        m_window.display();
+    }
+}
