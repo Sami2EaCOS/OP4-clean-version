ec7cce64991e5ca8f2befa3942204b6de6803d29
Sami MOURAD <33941598+Sami2EaCOS@users.noreply.github.com>
2018-11-30 17:03:35 +0100

    Create menuView.cpp

---
+++ src/views/menuView.cpp
@@ -0,0 +1,19 @@
+#include <views/menuView.hpp>
+
+#include <SFML/Graphics.hpp>
+
+MenuView :: MenuView() {
+
+}
+
+void MenuView :: update(sf::Time& time) {
+    
+}
+
+void MenuView :: render(sf::RenderWindow& window) {
+
+}
+
+void MenuView :: process_event(sf::Event& event, sf::Time& time) {
+    
+}
